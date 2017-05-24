9ef5876147ec900a4a29e690ac8864ea9522cc27
Pranshu Gupta <pranshug258@outlook.com>
2017-05-24 22:41:29 +0530

    made some minor changes

--- node/app/models/Student.js
+++ node/app/models/Student.js
@@ -11,17 +11,3 @@ var Student = mongoose.model('Student', studentSchema);
 module.exports = Student
 
 // String, Number, Date, Buffer, Boolean, Mixed, ObjectId, Array
-
-// mongoose.connect('mongodb://localhost:27017');
-// var std = new Student({
-//     name: "John",
-//     age: 25
-// })
-
-// std.save(function (err) {
-//     if (err) {
-//         console.log("An error occured: " + err)
-//     } else {
-//         console.log("The document was saved!")
-//     }
-// })
